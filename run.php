2780b0474b823447dd2f8b383938a1ec214eeffc
OhMyGumihooo <100387006+ohmygumihooo@users.noreply.github.com>
2022-04-30 05:47:26 +0700

    Update run.php

--- run.php
+++ run.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/userAgent.php';
 require __DIR__ . '/smshub.php';
 
 //APIKEY SMSHUB
-$key = '';
+$key = '128841U0ada0648553aafde2587b6d26f0f8c6e';
 
 if($key == null) {
     die('Masukan apikey di line 9 goblog!!');
